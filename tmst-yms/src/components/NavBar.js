f054bf48efa79763c251b7bfd0aac706fc916d8b
Ricardo Quintanar <lrq1012@gmail.com>
2019-07-02 09:13:17 -0500

    reorganized the folder structure

--- tmst-yms/src/components/NavBar.js
+++ tmst-yms/src/components/NavBar.js
@@ -9,7 +9,7 @@ class Navbar extends Component {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">View-Schedule</a>
+                            <a class="nav-link" href="#tmstrailers">View Schedule</a>
                         </li>
                     </ul>
                 </div>
