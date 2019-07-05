7d7f4dbcee8dde702b0b20e7daf07f4b4e730e67
Bettina Pulliam <bfpulliam@gmail.com>
2019-07-04 23:26:12 -0500

    Setting up json so that it runs properly on Heroku

--- tmst-yms/server.js
+++ tmst-yms/server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const app = express();
+const path = require("path");
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -27,5 +28,14 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+//Serve static items when in production
+if {process.env.NODE_ENV === "production"} {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+
+  });
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
